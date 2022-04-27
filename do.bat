7ed29ef3d5707d3ff523b64586a578dd5f301705
li-rongcheng <Li.Rongcheng@gmail.com>
2022-04-27 13:33:33 +1000

    update kotlin.bat path

--- do.bat
+++ do.bat
@@ -1,4 +1,5 @@
 @echo off
 ::C:\Users\Ron\AppData\Roaming\JetBrains\IdeaIC2020.3\plugins\Kotlin\kotlinc\bin\kotlin.bat -cp i:\sync_src\experiments\kotlin-github\kotlin-playground\out\production\kotlin-playground AppKt %*
-C:\Users\Ron\AppData\Roaming\JetBrains\IdeaIC2020.3\plugins\Kotlin\kotlinc\bin\kotlin.bat -cp out\production\kotlin-playground AppKt %*
+::C:\Users\Ron\AppData\Roaming\JetBrains\IdeaIC2020.3\plugins\Kotlin\kotlinc\bin\kotlin.bat -cp out\production\kotlin-playground AppKt %*
+C:\Users\liyan\AppData\Roaming\JetBrains\IdeaIC2021.3\plugins\Kotlin\kotlinc\bin\kotlin.bat -cp out\production\kotlin-playground AppKt %*
 @echo on
